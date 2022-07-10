62f6591da72a8dd12f5ab43c99032d3557d4e19e
sebastian castro cadavid <sebastianca105@hotmail.com>
2022-07-10 18:47:49 -0500

    Ajustes

---
+++ ApiXMen/Migrations/20220710234706_primary-key.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiXMen.Migrations
+{
+    public partial class primarykey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- ApiXMen/Models/DnaResult.cs
+++ ApiXMen/Models/DnaResult.cs
@@ -1,7 +1,10 @@
-﻿namespace ApiXMen.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiXMen.Models
 {
     public class DnaResult
     {
+        [Key]
         public string Id { get; set; }
         public string DnaVerified { get; set; }
         public bool TestResult { get; set; }
